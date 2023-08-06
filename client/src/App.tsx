246b11fc52beb0a1aeb59438bc119f6fdf2be54e
Lalasimon <lalaszymon@gmail.com>
2023-08-06 08:33:14 +0200

    Socket room connection working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,12 @@ import { io } from "socket.io-client";
 const socket = io("http://localhost:8080");
 
 const App = () => {
-    const [messages, setMessages] = useState<string[]>([]);
-    const [message, setMessage] = useState<string>("");
+    const [roomID, setRoomID] = useState("");
 
-    socket.on("message", (message: string) => {
-        setMessages([...messages, message]);
-    });
-
-    const sendMessage = () => {
-        socket.emit("message", message);
+    const joinRoom = () => {
+        if (roomID !== "") {
+            socket.emit("join_room", roomID);
+        }
     };
     return (
         <main>
@@ -21,23 +18,16 @@ const App = () => {
             <div className="flex justify-center gap-5 mt-10">
                 <input
                     className="p-1 border-2 border-black rounded-lg text-center"
-                    type="text"
-                    placeholder="type your message here"
-                    onChange={(e) => setMessage(e.target.value)}
+                    type="password"
+                    placeholder="Input room ID"
+                    onChange={(e) => setRoomID(e.target.value)}
                 />
                 <button
-                    onClick={sendMessage}
+                    onClick={joinRoom}
                     className="border-2 border-black rounded-lg px-4"
                 >
                     Confirm
                 </button>
-            </div>
-            <div className="flex gap-4 justify-center">
-                <ul className="flex flex-col gap-4 mt-10">
-                    {messages.map((message, index) => (
-                        <li key={index}>{message}</li>
-                    ))}
-                </ul>
             </div>
         </main>
     );
--- server/main.js
+++ server/main.js
@@ -7,7 +7,8 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
     },
 });
 
@@ -16,14 +17,14 @@ app.use(cors());
 app.get("/", (req, res) => {});
 
 io.on("connection", (socket) => {
-    console.log("a user connected");
-    socket.on("message", (msg) => {
-        console.log("message: " + msg);
-        io.emit("message", msg);
+    console.log("a user connected", socket.id);
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log("User joined room: " + data);
     });
 
     socket.on("disconnect", () => {
-        console.log("user disconnected");
+        console.log("user disconnected", socket.id);
     });
 });
 
