4ad4d5a85c132e3782f3f8f4c9e2422f4391271c
Szymon Lala <lalaszymon@gmail.com>
2023-08-28 18:50:31 +0200

    09 arrows - user can draw arrows for second user (#11)
    * arrows created, need to check infinite loop
    
    * working arrows for users on page

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -2,22 +2,34 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useParams, Link } from "react-router-dom";
 import { Chessboard } from "react-chessboard";
-import { Chess } from "chess.js";
+import { Chess, Square } from "chess.js";
 
 interface Move {
     from: string;
     to: string;
     promotion?: string;
 }
-
 const Room = () => {
     const [game, setGame] = useState(new Chess());
+    const [arrows, setArrows] = useState<Square[][]>([]);
     const { roomID } = useParams();
     const socket = io("http://localhost:8080");
 
+    socket.on("move_made", (move) => {
+        makeAMove({
+            from: move.from,
+            to: move.to,
+            promotion: "q",
+        });
+    });
+
+    socket.on("arrows_drawn", (arrowsData) => {
+        setArrows(arrowsData);
+    });
+
     useEffect(() => {
         socket.emit("join_room", roomID);
-    }, []);
+    });
 
     const leaveRoom = () => {
         socket.emit("leave_room", roomID);
@@ -45,13 +57,23 @@ const Room = () => {
         });
         return true;
     };
-    socket.on("move_made", (move) => {
-        makeAMove({
-            from: move.from,
-            to: move.to,
-            promotion: "q",
-        });
-    });
+
+    const arrowDrow = (arrowsData: Square[][]) => {
+        if (arrowsData.length === 0 && arrowsData !== arrows) {
+            setArrows([]);
+            socket.emit("draw_arrows", {
+                arrowsData,
+                roomID,
+            });
+        } else {
+            if (arrowsData.flat().join() === arrows.flat().join()) return;
+            setArrows(arrowsData);
+            socket.emit("draw_arrows", {
+                arrowsData,
+                roomID,
+            });
+        }
+    };
     return (
         <>
             <div className="flex flex-col">
@@ -63,6 +85,8 @@ const Room = () => {
                 <h1>Welcome on room {roomID}</h1>
                 <div>
                     <Chessboard
+                        customArrows={arrows}
+                        onArrowsChange={arrowDrow}
                         onPieceDrop={onDrop}
                         position={game.fen()}
                         boardWidth={650}
--- server/main.js
+++ server/main.js
@@ -3,7 +3,6 @@ const app = new Express();
 const http = require("http");
 const server = http.createServer(app);
 const port = 8080;
-const { Chess } = require("chess.js");
 const cors = require("cors");
 const { Server } = require("socket.io");
 const io = new Server(server, {
@@ -15,8 +14,6 @@ const io = new Server(server, {
 
 app.use(cors());
 
-app.get("/", (req, res) => {});
-
 io.on("connection", (socket) => {
     socket.on("join_room", (room) => {
         socket.join(room);
@@ -24,10 +21,16 @@ io.on("connection", (socket) => {
     socket.on("make_a_move", (data) => {
         socket.broadcast.to(data.roomID).emit("move_made", data.moveData);
     });
+    socket.on("draw_arrows", (data) => {
+        socket.to(data.roomID).emit("draw_arrows", data.arrows);
+    });
     socket.on("leave_room", (room) => {
         socket.leave(room);
     });
-    socket.on("disconnect", () => {});
+    socket.on("disconnect", (room) => {
+        socket.leave(room);
+        socket.disconnect();
+    });
 });
 
 server.listen(port, () => {
