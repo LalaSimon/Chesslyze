6359d8621fcb3941bb28cfdd940455b96552114c
Lalasimon <lalaszymon@gmail.com>
2023-09-29 21:43:44 +0200

    working move list, need to emit it

--- server/main.js
+++ server/main.js
@@ -2,45 +2,49 @@ const Express = require('express')
 const app = new Express()
 const http = require('http')
 const server = http.createServer(app)
-const port = 8080
+const port = 3000
 const cors = require('cors')
 const { Server } = require('socket.io')
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-  },
-})
 
-app.use(cors())
-
-app.get('/', (req, res) => {})
-
-io.on('connection', socket => {
-  socket.on('join_room', room => {
-    socket.join(room)
+const main = () => {
+  const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST'],
+    },
   })
-  socket.on('make_a_move', data => {
-    socket.broadcast.to(data.roomID).emit('move_made', data.moveData)
+  app.use(cors())
+  app.get('/', (req, res) => {})
+  io.on('connection', socket => {
+    socket.on('join_room', room => {
+      socket.join(room)
+    })
+    socket.on('make_a_move', data => {
+      console.log(data)
+      socket.broadcast.to(data.roomID).emit('move_made', data)
+    })
+    socket.on('draw_arrows', data => {
+      socket.broadcast.to(data.roomID).emit('arrows_drawn', data.arrowsData)
+    })
+    socket.on('clear_analyze', data => {
+      socket.broadcast.to(data.roomID).emit('analyze_cleared')
+    })
+    socket.on('send_highlight_square', data => {
+      socket.broadcast.to(data.roomID).emit('get_highlight_square', data.square)
+    })
+    socket.on('send_list_moves', data => {
+      socket.broadcast.to(data.roomID).emit('get_list_moves', data.listMoves)
+    })
+    socket.on('leave_room', room => {
+      socket.leave(room)
+    })
+    socket.on('disconnect', room => {
+      socket.leave(room)
+      socket.disconnect()
+    })
   })
-  socket.on('draw_arrows', data => {
-    socket.broadcast.to(data.roomID).emit('arrows_drawn', data.arrowsData)
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
   })
-  socket.on('send_highlight_square', data => {
-    socket.broadcast.to(data.roomID).emit('get_highlight_square', data.square)
-  })
-  socket.on('clear_highlight_squares', data => {
-    socket.to(data.roomID).emit('send_clear_highlight_squares')
-  })
-  socket.on('leave_room', room => {
-    socket.leave(room)
-  })
-  socket.on('disconnect', room => {
-    socket.leave(room)
-    socket.disconnect()
-  })
-})
-
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+}
+main()
