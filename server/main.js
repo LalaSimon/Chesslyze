c7624df30d02485c27ff275978ebd9f97df26fc4
Szymon Lala <lalaszymon@gmail.com>
2023-10-09 13:30:49 +0200

    backend code refactore due to lack of organisation (#21)

--- server/main.js
+++ server/main.js
@@ -1,59 +1,15 @@
+const socket = require('./socket/Socket')
 const Express = require('express')
 const app = new Express()
 const http = require('http')
 const server = http.createServer(app)
 const port = 3000
 const cors = require('cors')
-const { Server } = require('socket.io')
 
 const main = () => {
-  const io = new Server(server, {
-    cors: {
-      origin: 'http://localhost:5173',
-      methods: ['GET', 'POST'],
-    },
-  })
   app.use(cors())
+  socket(server)
   app.get('/', (req, res) => {})
-  io.on('connection', socket => {
-    socket.on('join_room', room => {
-      socket.join(room)
-    })
-    socket.on('make_a_move', data => {
-      socket.broadcast.to(data.roomID).emit('move_made', data)
-    })
-    socket.on('draw_arrows', data => {
-      socket.broadcast.to(data.roomID).emit('arrows_drawn', data.arrowsData)
-    })
-    socket.on('clear_analyze', data => {
-      socket.broadcast.to(data.roomID).emit('analyze_cleared')
-    })
-    socket.on('send_highlight_square', data => {
-      socket.broadcast.to(data.roomID).emit('get_highlight_square', data.square)
-    })
-    socket.on('send_list_moves', data => {
-      socket.broadcast.to(data.roomID).emit('get_list_moves', data.listMoves)
-    })
-    socket.on('set_game', data => {
-      socket.broadcast.to(data.roomID).emit('get_game', data.fen)
-    })
-    socket.on('clear_board', data => {
-      socket.broadcast.to(data.roomID).emit('board_cleared')
-    })
-    socket.on('change_orientation', data => {
-      socket.broadcast.to(data.roomID).emit('orientation_changed')
-    })
-    socket.on('other_player_orientation', data => {
-      socket.broadcast.to(data.roomID).emit('get_other_player_orientation', data.orientation)
-    })
-    socket.on('leave_room', room => {
-      socket.leave(room)
-    })
-    socket.on('disconnect', room => {
-      socket.leave(room)
-      socket.disconnect()
-    })
-  })
   server.listen(port, () => {
     console.log(`Server listening on port ${port}`)
   })
