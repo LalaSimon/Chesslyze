00f376b5330e11b4aaa747ccf3ae983fbfbe8e73
Szymon Lala <lalaszymon@gmail.com>
2023-10-13 09:22:29 +0200

    code refactor of homepage (#22)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import Room from './pages/Room.tsx'
+import Home from './pages/Home/Home.tsx'
+import Room from './pages/Room/Room.tsx'
 import './index.css'
-import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    RouterProvider,
-} from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-            <Route path="/" element={<App />} />
-            <Route path="/:roomID" element={<Room />} />
-        </>
-    )
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="/:roomID" element={<Room />} />
+    </>
+  )
 )
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
