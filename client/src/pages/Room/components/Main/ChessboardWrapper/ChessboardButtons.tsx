ce3be0608037d8a3a80cb55c6162a91dd81dbf2b
Szymon Lala <lalaszymon@gmail.com>
2024-01-29 19:00:00 +0100

    useRef into counter (#61)

--- client/src/pages/Room/components/Main/ChessboardWrapper/ChessboardButtons.tsx
+++ client/src/pages/Room/components/Main/ChessboardWrapper/ChessboardButtons.tsx
@@ -1,5 +1,5 @@
 import { Button } from '../../../../../shared/components/Button'
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useRef } from 'react'
 import { Chess } from 'chess.js'
 import { useTypedDispatch, useTypedSelector } from '../../../../../redux/store'
 import { clearFen, setUndoRedoFen } from '../../../../../redux/slices/fen'
@@ -17,13 +17,13 @@ type ChessboardButtonsProps = {
 export const ChessboardButtons = ({ setGame, roomID }: ChessboardButtonsProps) => {
   const { fen } = useTypedSelector(state => state.fen)
   const { moveCounter, moveList } = useTypedSelector(state => state.moveList)
-  const [showFenNumber, setShowFenNumber] = useState<number>(0)
+  const showFenNumber = useRef<number>(0)
   const dispatch = useTypedDispatch()
 
   const clearBoard = async () => {
     if (SocketService.socket) {
       GameService.cleanBoard(SocketService.socket, roomID)
-      setShowFenNumber(0)
+      showFenNumber.current = 0
       dispatch(clearFen())
       dispatch(clearMoveList())
       dispatch(setOpeningName(''))
@@ -42,27 +42,28 @@ export const ChessboardButtons = ({ setGame, roomID }: ChessboardButtonsProps) =
   }
 
   const handleUndo = () => {
-    if (!moveList[moveList.length - (2 - showFenNumber)] && SocketService.socket) {
+    if (!moveList[moveList.length - (2 - showFenNumber.current)] && SocketService.socket) {
       GameService.undoMove(SocketService.socket, roomID)
       dispatch(setUndoRedoFen(moveList[0].before))
-      setShowFenNumber(prevstate => --prevstate)
+      showFenNumber.current--
     } else if (SocketService.socket) {
       GameService.undoMove(SocketService.socket, roomID)
-      dispatch(setUndoRedoFen(moveList[moveList.length - (2 - showFenNumber)].after))
-      setShowFenNumber(prevstate => --prevstate)
+      dispatch(setUndoRedoFen(moveList[moveList.length - (2 - showFenNumber.current)].after))
+      showFenNumber.current--
     }
   }
 
   const handleRedo = () => {
     if (SocketService.socket) {
       GameService.redoMove(SocketService.socket, roomID)
-      setShowFenNumber(prevstate => ++prevstate)
-      dispatch(setUndoRedoFen(moveList[moveList.length + showFenNumber].after))
+      dispatch(setUndoRedoFen(moveList[moveList.length + showFenNumber.current].after))
+      showFenNumber.current++
     }
   }
 
   useEffect(() => {
     const clearBoardHandler = async () => {
+      showFenNumber.current = 0
       setGame(new Chess())
       dispatch(clearFen())
       dispatch(clearMoveList())
@@ -71,18 +72,18 @@ export const ChessboardButtons = ({ setGame, roomID }: ChessboardButtonsProps) =
     }
 
     const undoMoveHandler = () => {
-      if (!moveList[moveList.length - (2 - showFenNumber)]) {
+      if (!moveList[moveList.length - (2 - showFenNumber.current)]) {
         dispatch(setUndoRedoFen(moveList[0].before))
-        setShowFenNumber(prevstate => --prevstate)
+        showFenNumber.current--
       } else {
-        dispatch(setUndoRedoFen(moveList[moveList.length - (2 - showFenNumber)].after))
-        setShowFenNumber(prevstate => --prevstate)
+        dispatch(setUndoRedoFen(moveList[moveList.length - (2 - showFenNumber.current)].after))
+        showFenNumber.current--
       }
     }
 
     const redoMoveHandler = () => {
-      setShowFenNumber(prevstate => ++prevstate)
-      dispatch(setUndoRedoFen(moveList[moveList.length + showFenNumber].after))
+      dispatch(setUndoRedoFen(moveList[moveList.length + showFenNumber.current].after))
+      showFenNumber.current++
     }
 
     if (SocketService.socket) GameService.onCleanBoardUpdate(SocketService.socket)
@@ -104,11 +105,11 @@ export const ChessboardButtons = ({ setGame, roomID }: ChessboardButtonsProps) =
   return (
     <section className="flex gap-5">
       <Button
-        disabled={moveCounter === -1 || !moveList[moveList.length - (1 - showFenNumber)]}
+        disabled={moveCounter === -1 || !moveList[moveList.length - (1 - showFenNumber.current)]}
         callback={handleUndo}
         btnText="Undo"
       />
-      <Button disabled={showFenNumber === 0} callback={handleRedo} btnText="Redo" />
+      <Button disabled={showFenNumber.current === 0} callback={handleRedo} btnText="Redo" />
       <Button callback={clearBoard} btnText="Clear" />
       <Button callback={copyFen} btnText="Copy fen" />
     </section>
