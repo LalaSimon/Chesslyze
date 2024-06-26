eb7950d9d7579ba1601025e7daba5afb70cf03f8
Lalasimon <lalaszymon@gmail.com>
2024-06-26 10:10:20 +0200

    fix with remembered fix on client

--- client/src/pages/Home/components/Main.tsx
+++ client/src/pages/Home/components/Main.tsx
@@ -3,9 +3,14 @@ import { useNavigate } from 'react-router-dom'
 import SocketService from '@services/SocketService'
 import GameService from '@services/GameService'
 import { Button } from '@shared/components/Button'
+import { useTypedDispatch } from '@redux/store'
+import { clearMoveList } from '@redux/slices/Analysis/moveList'
+import { clearOpeningList, clearOpeningName } from '@redux/slices/Analysis/openingInfo'
+import { clearFen } from '@redux/slices/Analysis/fen'
 
 export const Main = () => {
   const [roomID, setRoomID] = useState('')
+  const dispatch = useTypedDispatch()
   const navigate = useNavigate()
   const socketURL = 'http://localhost:3000'
 
@@ -26,6 +31,10 @@ export const Main = () => {
     const socket = SocketService.socket
     if (!roomID || !socket) return
     GameService.joinRoom(socket, roomID, navigate)
+    dispatch(clearMoveList())
+    dispatch(clearOpeningName())
+    dispatch(clearOpeningList())
+    dispatch(clearFen())
   }
   return (
     <main className="mt-10 flex justify-center gap-5">
